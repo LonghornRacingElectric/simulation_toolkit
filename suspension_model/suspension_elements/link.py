6bef4b46e6f453464b7439393e5273adaae1a690
robert <roberthorvath5@gmail.com>
2024-08-15 01:31:56 -0500

    Added kinematic RCs for initial visualization

--- suspension_model/suspension_elements/link.py
+++ suspension_model/suspension_elements/link.py
@@ -29,6 +29,35 @@ class Link:
         for element in self.elements:
             element.plot_elements(plotter=plotter)
     
+    def yz_intersection(self, link: "Link"):
+        l_1i = self.inboard_node.position
+        l_1o = self.outboard_node.position
+        m_1 = (l_1o[2] - l_1i[2]) / (l_1o[1] - l_1i[1])
+        y_1, z_1 = l_1o[1], l_1o[2]
+
+        l_2i = link.inboard_node.position
+        l_2o = link.outboard_node.position
+        m_2 = (l_2o[2] - l_2i[2]) / (l_2o[1] - l_2i[1])
+        y_2, z_2 = l_2o[1], l_2o[2]
+
+        a = np.array([
+            [-1 * m_1, 1],
+            [-1 * m_2, 1]
+        ])
+
+        b = np.array([
+            [-1 * m_1 * y_1 + z_1],
+            [-1 * m_2 * y_2 + z_2]
+        ])
+
+        y, z = np.linalg.solve(a=a, b=b)
+
+        # Calculate x-value
+        # I'll average between left and right halves for KinRC
+        x = np.average([l_1o[0], l_2o[0]])
+
+        return np.array([x, y[0], z[0]])
+    
     @property
     def direction(self) -> np.ndarray:
         return unit_vec(self.outboard_node, self.inboard_node)
--- suspension_model/suspension_model.py
+++ suspension_model/suspension_model.py
@@ -1,4 +1,6 @@
 from suspension_model.suspension_elements.double_wishbone import DoubleWishbone
+from suspension_model.suspension_elements.link import Link
+from suspension_model.suspension_elements.node import Node
 from suspension_model.assets.plotter import Plotter
 from typing import Callable
 from typing import Sequence
@@ -43,6 +45,7 @@ class SuspensionModel:
         
         # Initialize plotter
         self.plotter = plotter
+        self.verbose: bool = False
 
         # Initialize each corner assembly
         self.FL_double_wishbone: DoubleWishbone = DoubleWishbone(inboard_points=FL_inboard_points,
@@ -81,30 +84,53 @@ class SuspensionModel:
                                                                 tire_width=tire_width,
                                                                 show_ICs=show_ICs)
         
+        # Kin RC shit as a proof of concept
+        self.Fr_KinRC: Node = Node(position=self.calc_Fr_KinRC())
+        self.Rr_KinRC: Node = Node(position=self.calc_Rr_KinRC())
+        self.roll_axis: Link = Link(inboard=self.Fr_KinRC, outboard=self.Rr_KinRC)
+        
         self.elements = [self.FL_double_wishbone, self.FR_double_wishbone, self.RL_double_wishbone, self.RR_double_wishbone]
+        self.hidden_elements = [self.Fr_KinRC, self.Rr_KinRC, self.roll_axis]
+
+    def calc_Fr_KinRC(self):
+        return self.FL_double_wishbone.FVIC_link.yz_intersection(link=self.FR_double_wishbone.FVIC_link)
+    
+    def calc_Rr_KinRC(self):
+        return self.RL_double_wishbone.FVIC_link.yz_intersection(link=self.RR_double_wishbone.FVIC_link)
     
     def steer(self, rack_displacement: float):
         self.FL_double_wishbone.steer(rack_displacement)
         self.FR_double_wishbone.steer(rack_displacement)
+        
+        self.Fr_KinRC.position = self.calc_Fr_KinRC()
+        self.Rr_KinRC.position = self.calc_Rr_KinRC()
     
     def jounce(self, jounce: float):
         self.FL_double_wishbone.jounce(jounce)
-        self.FR_double_wishbone.jounce(jounce)
+        self.FR_double_wishbone.jounce(-1 * jounce)
         self.RL_double_wishbone.jounce(jounce)
-        self.RR_double_wishbone.jounce(jounce)
+        self.RR_double_wishbone.jounce(-1 * jounce)
 
-    def plot_elements(self, plotter):
+        self.Fr_KinRC.position = self.calc_Fr_KinRC()
+        self.Rr_KinRC.position = self.calc_Rr_KinRC()
+
+    def plot_elements(self, plotter, verbose):
+        self.verbose = verbose
         plotter.clear()
         for corner in self.elements:
             corner.plot_elements(plotter=plotter)
+        
+        if verbose:
+            for element in self.hidden_elements:
+                element.plot_elements(plotter=plotter)
 
     def add_slider(self, func: Callable, title: str, bounds: Tuple[float, float], pos: Tuple[Tuple[float, float], Tuple[float, float]]):
         self.plotter.add_slider(func, title, bounds, pos)
 
     def steer_slider(self, steer: float):
         self.steer(rack_displacement=steer)
-        self.plot_elements(plotter=self.plotter)
+        self.plot_elements(plotter=self.plotter, verbose=self.verbose)
 
     def jounce_slider(self, jounce: float):
         self.jounce(jounce=jounce)
-        self.plot_elements(plotter=self.plotter)
+        self.plot_elements(plotter=self.plotter, verbose=self.verbose)
