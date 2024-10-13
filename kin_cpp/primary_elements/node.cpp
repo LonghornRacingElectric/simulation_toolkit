3e96cf01b2fa71f7397afd8d790ae79a10164368
abatra <linkarnav@gmail.com>
2024-10-13 11:49:50 -0500

    minor node changes

--- kin_cpp/primary_elements/node.cpp
+++ kin_cpp/primary_elements/node.cpp
@@ -2,7 +2,13 @@
 #include "node.h"
 using namespace std;
 
-// Node constructor
+Node (array<double, 3>);
+void reset ();
+void translate(array<double, 3>);
+void flatten_rotate(array<double, 3>);
+
+/* constructor for Node object 
+   Parameters : pos (3-element array) -- x, y, z*/
 Node::Node (array<double, 3> pos) {
     //copy coordinates into position
     for (int i = 0; i < 3; i++)
@@ -10,3 +16,5 @@ Node::Node (array<double, 3> pos) {
         position[i] = pos[i];
     } 
 }
+
+/*  */
