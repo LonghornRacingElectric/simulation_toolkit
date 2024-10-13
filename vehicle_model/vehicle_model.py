328981204843e55049b12d68e3835ffe309de0fc
Robert Horvath <rhorvath@utexas.edu>
2024-10-13 15:45:32 -0500

    Updated YMD. More updates coming soon

--- vehicle_model/vehicle_model.py
+++ vehicle_model/vehicle_model.py
@@ -114,7 +114,7 @@ class VehicleModel:
             show_ICs=False,
             plotter=self.suspension_plotter)
         
-        # self.suspension.generate_kin()
+        self.suspension.generate_kin()
 
         # self.suspension.generate_kin_plots(steer_sweep=np.linspace(-1.5, 1.5, 26) * 0.0254, 
         #                                    heave_sweep=np.linspace(-4, 4, 26) * 0.0254,
