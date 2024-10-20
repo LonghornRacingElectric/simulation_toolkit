b460c8e0ed62648284eed8eac44d3a9ac4abb645
keertan12345 <keertan12345@gmail.com>
2024-10-20 11:02:02 -0500

    changes to node.cpp

--- kin_cpp/primary_elements/node.cpp
+++ kin_cpp/primary_elements/node.cpp
@@ -22,6 +22,18 @@ void Node::translate(StaticVector<double, 3UL> &translation) {
     //TODO: requires plotting library
 }
 
-void Node::flatten_rotate(StaticVector<double, 3UL> &rotation) {
-    //TODO: requires linear alg library
+void Node::flatten_rotate(StaticVector<double, 3UL> &rotation, double theta) {
+    StaticMatrix<double, 3UL, 3UL> x_rot = rotation_matrix(StaticVector<double, 3UL>{1, 0, 0}, rotation[0]);
+    StaticMatrix<double, 3UL, 3UL> y_rot = rotation_matrix(StaticVector<double, 3UL>{0, 1, 0}, rotation[1]);
+    StaticMatrix<double, 3UL, 3UL> z_rot = rotation_matrix(StaticVector<double, 3UL>{0, 0, 1}, rotation[2]);
+
+    // Apply the rotation: z_rot * y_rot * x_rot * position
+    // NOTE: The order of multiplication switches because matrix multiplication in blaze applies transformations in reverse order.
+    // This is due to the nature of matrix multiplication: transformations are applied from right to left, meaning
+    // z_rot * y_rot * x_rot * position will rotate the position vector first by the x-axis, then y-axis, then z-axis.
+    position = z_rot * (y_rot * (x_rot * position));
+
+
+
+    
 }
