3d9528fe4a9526afccf21363ca1a26a9b60abec1
Holden <holden@wireless-10-145-71-189.public.utexas.edu>
2024-10-27 12:03:09 -0500

    Fixed typos

--- kin_cpp/tertiary_elements/push_pull_rod.cpp
+++ kin_cpp/tertiary_elements/push_pull_rod.cpp
@@ -2,7 +2,8 @@
 #include "../assets/misc_linalg.h"
 
 PushPullRod::PushPullRod (Node *inboard, Node *outboard, bool upper, bool bellcrank, Node *bc_pivot, StaticVector<double, 3UL> &bc_direction, Node *shock_outboard, Node *shock_inboard) {
-    rod = &Beam (inboard, outboard);
+    // rod = &Beam (inboard, outboard);
+    Beam* rod = new Beam(inboard, outboard);
     initial_rod_length = rod_length ();
     bellcrank_angle = 0.0;
     wishbone_angle = 0.0;
@@ -11,8 +12,8 @@ PushPullRod::PushPullRod (Node *inboard, Node *outboard, bool upper, bool bellcr
     if (bc_exists) {
         bellcrank_pivot = bc_pivot;
         bellcrank_direction = bc_direction;
-        Beam *damper_beam = &Beam (shock_inboard, shock_outboard);
-        damper = &Damper (damper_beam);
+        Beam *damper_beam = new Beam (shock_inboard, shock_outboard);
+        damper = new Damper (damper_beam);
         initial_spring_damper_length = spring_damper_length ();
     }
 
