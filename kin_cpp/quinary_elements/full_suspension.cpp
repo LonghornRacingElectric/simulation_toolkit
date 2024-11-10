9bce0b9c0dd5e9117dc90466aa56bf7302cf737a
keertan12345 <keertan12345@gmail.com>
2024-11-10 14:54:46 -0600

    full sus'

--- kin_cpp/quinary_elements/full_suspension.cpp
+++ kin_cpp/quinary_elements/full_suspension.cpp
@@ -146,3 +146,113 @@ double FullSuspension::heave_stiffness () const {
 
     return FL_wheelrate + FR_wheelrate + RL_wheelrate + RR_wheelrate;
 }
+<<<<<<< Updated upstream
+=======
+
+double FullSuspension::roll_stiffness () const {
+    double Fr_axle_rate = Fr_axle->roll_stiffness;
+    double Rr_axle_rate = Rr_axle->roll_stiffness;
+
+    return Fr_axle_rate + Rr_axle_rate;
+}
+
+double FullSuspension::pitch_stiffness () const {
+    double Fr_axle_rate = Fr_axle->left->wheelrate + Fr_axle->right->wheelrate;
+    double Rr_axle_rate = Rr_axle->left->wheelrate + Rr_axle->right->wheelrate;
+    double Fr_position = Fr_axle->left->contact_patch.->position[0];
+    double Rr_position = Rr_axle->left->contact_patch.->position[0];
+    double cg_position = cg->position[0];
+
+    double pitch_stiffness = 1/4 * ((Fr_position - cg_position)**2 * Fr_axle_rate + (Rr_position - cg_position)**2 * Rr_axle_rate);
+    return pitch_stiffness;
+}
+
+void FullSuspension::reset_position () {
+    translate(-1 * prac_average_cp);
+    flatten_rotate({ang_x, -1 * ang_y, 0});
+    translate(current_average_cp);
+}
+
+void FullSuspension::hard_reset () {
+    steer(0.000001);
+    heave(0.000001);
+    pitch(0.000001);
+    roll(0.000001);
+
+    *this = FullSuspension (this->Fr_axle, this->Rr_axle, this->cg);
+}
+
+void FullSuspension::flatten () {
+    vector<double> FL_cp = Fr_axle->left->position;
+    vector<double> FR_cp = Fr_axle->right->position;
+    vector<double> RL_cp = Rr_axle->left->position;
+    vector<double> RR_cp = Rr_axle->right->position;
+
+    vector<double> average_cp = {(FL_cp[0] + FR_cp[0] + FL_cp[0] + RR_cp[0]) / 4,
+                                (FL_cp[1] + FR_cp[1] + FL_cp[1] + RR_cp[1]) / 4,
+                                (FL_cp[2] + FR_cp[2] + FL_cp[2] + RR_cp[2]) / 4};
+
+    this-> current_average_cp = average_cp;
+    translate(-1*average_cp);
+    auto plane_enq = 
+
+    auto plane_coeffs = plane({FL_cp, FR_cp, RL_cp});
+        double a = plane_coeffs[0], b = plane_coeffs[1], c = plane_coeffs[2];
+        double x_0 = plane_coeffs[3], y_0 = plane_coeffs[4], z_0 = plane_coeffs[5];
+
+    auto plane_eqn = [&](double x, double y) -> double {
+            return ((a * x_0 + b * y_0 + c * z_0) - a * x - b * y) / c;
+        };
+
+    ang_x = atan(plane_eqn(0,1));
+    ang_y = atan(plane_eqn(1,0));
+
+    if (transform_origin) {
+        flatten_rotate({-ang_x, ang_y, 0.0});
+    }
+
+    prac_average_cp = {current_average_cp[0], current_average_cp[1], 0.0};
+    translate(prac_average_cp);
+}
+
+vector<double, 6> FullSuspension::plane (vector<vector<double>> points) {
+    assert(points.size() == 3 && "Plane generator only accepts 3 points");
+        vector<double> PQ = {points[1][0] - points[0][0], points[1][1] - points[0][1], points[1][2] - points[0][2]};
+        vector<double> PR = {points[2][0] - points[0][0], points[2][1] - points[0][1], points[2][2] - points[0][2]};
+        
+        // Calculates cross product or PQ and PR
+        vector<double, 3> normal;
+        normal[0] = PQ[1] * PR[2] - PQ[2] * PR[1]; // i-component
+        normal[1] = PQ[2] * PR[0] - PQ[0] * PR[2]; // j-component
+        normal[2] = PQ[0] * PR[1] - PQ[1] * PR[0]; // k-component
+
+        double a = normal[0];
+        double b = normal[1];
+        double c = normal[2];
+
+        double x_0 = points[0][0];
+        double y_0 = points[0][1];
+        double z_0 = points[0][2];
+
+        return {a, b, c, x_0, y_0, z_0};
+}
+
+void FullSuspension::translate(vector<double> translation) {
+    for (int i=0; i<all_elements.size(); i++) {
+        all_elements[i].translate(translation);
+    }
+}
+
+void FullSuspension::flatten_rotate (vector<double> angle) {
+    for (int i=0; i<all_elements.size(); i++) {
+        all_elements[i].flatten_rotate(angle);
+    }
+}
+
+//TODO
+void FullSuspension::plot_elements() {
+
+}
+       
+
+>>>>>>> Stashed changes
