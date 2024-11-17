c0a3c658662a60a67219aae66e32d30eccf98065
Robert Horvath <roberthorvath5@gmail.com>
2024-11-17 16:17:09 -0600

    added python unit tests

---
+++ unit_tests/test_link.py
@@ -0,0 +1,151 @@
+from vehicle_model.new_suspension_model.suspension_elements.primary_elements.link import Link
+from vehicle_model.new_suspension_model.suspension_elements.primary_elements.node import Node
+from vehicle_model.new_suspension_model.assets.plotter import Plotter
+import numpy as np
+
+from unittest import main, TestCase
+
+
+class LinkTest(TestCase):
+    def test_link_init_direction_value(self):
+        inboard = Node(position=[-1, 0, 0])
+        outboard = Node(position=[1, 0, 0])
+        test_link = Link(inboard=inboard, outboard=outboard)
+
+        self.assertListEqual(list(test_link.direction), [1, 0, 0])
+    
+    def test_link_init_direction_type(self):
+        inboard = Node(position=[-1, 0, 0])
+        outboard = Node(position=[1, 0, 0])
+        test_link = Link(inboard=inboard, outboard=outboard)
+
+        self.assertIsInstance(test_link.direction, np.ndarray)
+    
+    def test_link_init_center_value(self):
+        inboard = Node(position=[-3, 0, 0])
+        outboard = Node(position=[1, 0, 0])
+        test_link = Link(inboard=inboard, outboard=outboard)
+
+        self.assertListEqual(list(test_link.center), [-1, 0, 0])
+
+    def test_link_init_center_type(self):
+        inboard = Node(position=[-3, 0, 0])
+        outboard = Node(position=[1, 0, 0])
+        test_link = Link(inboard=inboard, outboard=outboard)
+
+        self.assertIsInstance(test_link.center, np.ndarray)
+
+    def test_link_init_height_value(self):
+        inboard = Node(position=[-3, 0, 0])
+        outboard = Node(position=[1, 0, 0])
+        test_link = Link(inboard=inboard, outboard=outboard)
+
+        self.assertEqual(test_link.height, 4)
+
+    def test_link_init_height_type(self):
+        inboard = Node(position=[-3, 0, 0])
+        outboard = Node(position=[1, 0, 0])
+        test_link = Link(inboard=inboard, outboard=outboard)
+
+        self.assertIsInstance(test_link.height, float)
+
+    # def test_node_translate_position(self):
+    #     test_node = Node(position=[1, 1, 1])
+    #     test_node.translate(translation=[-1, -1, -1])
+        
+    #     self.assertListEqual(list(test_node.position), [0, 0, 0])
+
+    # def test_node_translate_type(self):
+    #     test_node = Node(position=[1, 1, 1])
+    #     test_node.translate(translation=[-1, -1, -1])
+
+    #     self.assertIsInstance(test_node.position, np.ndarray)
+
+    # def test_node_translate_initial_position(self):
+    #     test_node = Node(position=[1, 1, 1])
+    #     test_node.translate(translation=[-1, -1, -1])
+        
+    #     self.assertListEqual(list(test_node.initial_position), [1, 1, 1])
+
+    # def test_node_rotate_base(self):
+    #     origin_node = Node(position=[0, 0, 0])
+    #     update_node = Node(position=[1, 1, 0])
+
+    #     update_node.rotate(origin=origin_node, angle_z=np.pi)
+    #     self.assertListEqual(list([round(float(x)) for x in update_node.position]), [-1, -1, 0])
+
+    # def test_node_rotate_edge(self):
+    #     origin_node = Node(position=[0, 0, 0])
+    #     update_node = Node(position=[0, 0, 0])
+
+    #     update_node.rotate(origin=origin_node, angle_z=np.pi)
+    #     self.assertListEqual(list([round(float(x)) for x in update_node.position]), [0, 0, 0])
+    
+    # def test_node_rotate_ND(self):
+    #     origin_node = Node(position=[1, 1, 1])
+    #     update_node = Node(position=[0, 0, 0])
+
+    #     update_node.rotate(origin=origin_node, angle_x=np.pi, angle_y=np.pi/2)
+    #     self.assertListEqual(list([round(float(x)) for x in update_node.position]), [2, 2, 2])
+    
+    # def test_node_reset_position(self):
+    #     origin_node = Node(position=[1, 1, 1])
+    #     update_node = Node(position=[0, 0, 0])
+
+    #     update_node.translate(translation=[-1, -1, -1])
+    #     update_node.rotate(origin=origin_node, angle_x=np.pi, angle_y=np.pi/2)
+    #     update_node.reset()
+
+    #     self.assertListEqual(list(update_node.position), [0, 0, 0])
+    
+    # def test_node_reset_type(self):
+    #     origin_node = Node(position=[1, 1, 1])
+    #     update_node = Node(position=[0, 0, 0])
+
+    #     update_node.translate(translation=[-1, -1, -1])
+    #     update_node.rotate(origin=origin_node, angle_x=np.pi, angle_y=np.pi/2)
+    #     update_node.reset()
+
+    #     self.assertIsInstance(update_node.position, np.ndarray)
+    
+    # def test_plotter_base(self):
+    #     test_plotter = Plotter()
+
+    #     test_node_1 = Node(position=[1, 1, 1])
+    #     test_node_2 = Node(position=[1, -1, 1])
+    #     test_node_3 = Node(position=[-1, 1, 1])
+    #     test_node_4 = Node(position=[-1, -1, 1])
+    #     test_node_5 = Node(position=[1, 1, -1])
+    #     test_node_6 = Node(position=[1, -1, -1])
+    #     test_node_7 = Node(position=[-1, 1, -1])
+    #     test_node_8 = Node(position=[-1, -1, -1])
+
+    #     nodes = [test_node_1, test_node_2, test_node_3, test_node_4, test_node_5, test_node_6, test_node_7, test_node_8]
+
+    #     for node in nodes:
+    #         node.plot(plotter=test_plotter, radius=0.25)
+    #     # test_plotter.show()
+    
+    # def test_plotter_transform(self):
+    #     test_plotter = Plotter()
+
+    #     test_node_1 = Node(position=[1, 1, 1])
+    #     test_node_2 = Node(position=[1, -1, 1])
+    #     test_node_3 = Node(position=[-1, 1, 1])
+    #     test_node_4 = Node(position=[-1, -1, 1])
+    #     test_node_5 = Node(position=[1, 1, -1])
+    #     test_node_6 = Node(position=[1, -1, -1])
+    #     test_node_7 = Node(position=[-1, 1, -1])
+    #     test_node_8 = Node(position=[-1, -1, -1])
+
+    #     nodes = [test_node_1, test_node_2, test_node_3, test_node_4, test_node_5, test_node_6, test_node_7, test_node_8]
+
+    #     for node in nodes:
+    #         node.plot(plotter=test_plotter, radius=0.25)
+
+    #     for node in nodes:
+    #         node.rotate(origin=Node(position=[0, 0, 0]), angle_x=np.pi/4, angle_y=np.pi/4)
+
+    #     for node in nodes:
+    #         node.plot(plotter=test_plotter, radius=0.25)
+    #     # test_plotter.show()
--- unit_tests/test_node.py
+++ unit_tests/test_node.py
@@ -1,11 +1,119 @@
-from vehicle_model.suspension_model.suspension_elements.primary_elements.node import Node
+from vehicle_model.new_suspension_model.suspension_elements.primary_elements.node import Node
+from vehicle_model.new_suspension_model.assets.plotter import Plotter
+import numpy as np
+
 from unittest import main, TestCase
 
 
 class NodeTest(TestCase):
-    def test_node(self):
-        self.assertEqual(1, 1)
-        
+    def test_node_init_position(self):
+        test_node = Node(position=[1, 1, 1])
 
-if __name__ == "__main__":
-    main()
+        self.assertListEqual(list(test_node.position), [1, 1, 1])
+        self.assertIsInstance(test_node.position, np.ndarray)
+    
+    def test_node_init_type(self):
+        test_node = Node(position=[1, 1, 1])
+
+        self.assertIsInstance(test_node.position, np.ndarray)
+
+    def test_node_translate_position(self):
+        test_node = Node(position=[1, 1, 1])
+        test_node.translate(translation=[-1, -1, -1])
+        
+        self.assertListEqual(list(test_node.position), [0, 0, 0])
+
+    def test_node_translate_type(self):
+        test_node = Node(position=[1, 1, 1])
+        test_node.translate(translation=[-1, -1, -1])
+
+        self.assertIsInstance(test_node.position, np.ndarray)
+
+    def test_node_translate_initial_position(self):
+        test_node = Node(position=[1, 1, 1])
+        test_node.translate(translation=[-1, -1, -1])
+        
+        self.assertListEqual(list(test_node.initial_position), [1, 1, 1])
+
+    def test_node_rotate_base(self):
+        origin_node = Node(position=[0, 0, 0])
+        update_node = Node(position=[1, 1, 0])
+
+        update_node.rotate(origin=origin_node, angle_z=np.pi)
+        self.assertListEqual(list([round(float(x)) for x in update_node.position]), [-1, -1, 0])
+
+    def test_node_rotate_edge(self):
+        origin_node = Node(position=[0, 0, 0])
+        update_node = Node(position=[0, 0, 0])
+
+        update_node.rotate(origin=origin_node, angle_z=np.pi)
+        self.assertListEqual(list([round(float(x)) for x in update_node.position]), [0, 0, 0])
+    
+    def test_node_rotate_ND(self):
+        origin_node = Node(position=[1, 1, 1])
+        update_node = Node(position=[0, 0, 0])
+
+        update_node.rotate(origin=origin_node, angle_x=np.pi, angle_y=np.pi/2)
+        self.assertListEqual(list([round(float(x)) for x in update_node.position]), [2, 2, 2])
+    
+    def test_node_reset_position(self):
+        origin_node = Node(position=[1, 1, 1])
+        update_node = Node(position=[0, 0, 0])
+
+        update_node.translate(translation=[-1, -1, -1])
+        update_node.rotate(origin=origin_node, angle_x=np.pi, angle_y=np.pi/2)
+        update_node.reset()
+
+        self.assertListEqual(list(update_node.position), [0, 0, 0])
+    
+    def test_node_reset_type(self):
+        origin_node = Node(position=[1, 1, 1])
+        update_node = Node(position=[0, 0, 0])
+
+        update_node.translate(translation=[-1, -1, -1])
+        update_node.rotate(origin=origin_node, angle_x=np.pi, angle_y=np.pi/2)
+        update_node.reset()
+
+        self.assertIsInstance(update_node.position, np.ndarray)
+    
+    def test_plotter_base(self):
+        test_plotter = Plotter()
+
+        test_node_1 = Node(position=[1, 1, 1])
+        test_node_2 = Node(position=[1, -1, 1])
+        test_node_3 = Node(position=[-1, 1, 1])
+        test_node_4 = Node(position=[-1, -1, 1])
+        test_node_5 = Node(position=[1, 1, -1])
+        test_node_6 = Node(position=[1, -1, -1])
+        test_node_7 = Node(position=[-1, 1, -1])
+        test_node_8 = Node(position=[-1, -1, -1])
+
+        nodes = [test_node_1, test_node_2, test_node_3, test_node_4, test_node_5, test_node_6, test_node_7, test_node_8]
+
+        for node in nodes:
+            node.plot(plotter=test_plotter, radius=0.25)
+        # test_plotter.show()
+    
+    def test_plotter_transform(self):
+        test_plotter = Plotter()
+
+        test_node_1 = Node(position=[1, 1, 1])
+        test_node_2 = Node(position=[1, -1, 1])
+        test_node_3 = Node(position=[-1, 1, 1])
+        test_node_4 = Node(position=[-1, -1, 1])
+        test_node_5 = Node(position=[1, 1, -1])
+        test_node_6 = Node(position=[1, -1, -1])
+        test_node_7 = Node(position=[-1, 1, -1])
+        test_node_8 = Node(position=[-1, -1, -1])
+
+        nodes = [test_node_1, test_node_2, test_node_3, test_node_4, test_node_5, test_node_6, test_node_7, test_node_8]
+
+        for node in nodes:
+            node.plot(plotter=test_plotter, radius=0.25)
+
+        for node in nodes:
+            node.rotate(origin=Node(position=[0, 0, 0]), angle_x=np.pi/4, angle_y=np.pi/4)
+
+        for node in nodes:
+            node.plot(plotter=test_plotter, radius=0.25)
+        # test_plotter.show()
