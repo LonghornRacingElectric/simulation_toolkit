c55a793319db6dd154f6d4cce7eee7c58800c3ea
keertan12345 <keertan12345@gmail.com>
2024-12-08 16:49:32 -0600

    unit tests for node

--- unit_tests/test_node.cpp
+++ unit_tests/test_node.cpp
@@ -1,7 +1,7 @@
-#include <gtest/gtest.h>
+#include "../kin_cpp/third_party/googletest/googletest/include/gtest/gtest.h"
 #include <blaze/Math.h>
 #include <kin_cpp/primary_elements/node.h>
-#include "../assets/misc_linalg.h"
+#include "../kin_cpp/assets/misc_linalg.h"
 #include "test_node.h"
 
 using namespace blaze;
